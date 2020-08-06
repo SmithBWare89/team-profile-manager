14dc8cd4e9b1bcb20b40fa3721d520382df96d98
Stephon Smith <smithwrestling89@gmail.com>
2020-08-06 12:44:03 -0400

    added additional testing for officeNumber input

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -4,13 +4,13 @@ test('that the managers number is set', () => {
     const manager = new Manager(5558675309);
 
     expect(manager.officeNumber).toEqual(expect.any(Number));
-})
+});
 
 test('that the managers number is 10 digits', () => {
     const manager = new Manager(5558675309);
 
     expect(manager.officeNumber.toString()).toHaveLength(10);
-})
+});
 
 test('if the employee information extends to Manager', () => {
     const manager = new Manager(5558675309);
@@ -19,10 +19,29 @@ test('if the employee information extends to Manager', () => {
     expect(manager.email).toEqual(expect.any(String));
     expect(manager.role).toEqual(expect.any(String));
     expect(manager.id).toEqual(expect.any(Number));
-})
+});
 
 test('if get role returns the updated role', () => {
     const manager = new Manager(5558675309);
 
     expect(manager.role).toEqual('Manager');
+});
+
+test('if the error is console logged when type of number is a string', () => {
+    const manager = new Manager('5558675309');
+
+    expect("Office number must be a number!");
+})
+
+test('if the error is console logged when input is less than 0', () => {
+    const manager = new Manager(-1500);
+
+    expect("Office number must be a positive number!");
+})
+
+test('if the error is console logged when length is less than or greater than 10', () => {
+    const manager = new Manager(5558609);
+    const manager2 = new Manager(123132123132132);
+
+    expect("Office number must be 10 digits");
 })
